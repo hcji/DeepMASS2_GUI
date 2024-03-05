95fb7ddd68e7099d20975ea09e24522dd158a11e
lkr <2672458098@qq.com>
2024-03-05 14:02:50 +0800

    feat:部署时自动从github拉取最新代码

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 pwd
+git remote add origin https://github.com/hcji/DeepMASS2_GUI
+git reset --hard origin/lkr_dev
 source activate deepmass2
 python ./backend/gradio_app.py & python ./backend/register.py
