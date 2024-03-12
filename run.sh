74e7485225456a34273bc1e1574009f9d8ddf2be
lkr <2672458098@qq.com>
2024-03-12 19:14:13 +0800

    fix:新增kill残余进程

--- run.sh
+++ run.sh
@@ -3,4 +3,6 @@ pwd
 git remote add origin https://github.com/hcji/DeepMASS2_GUI
 git reset --hard origin/lkr_dev
 source activate deepmass2
+lsof -i:12341 | grep 'TCP' | awk '{print $2}' | xargs kill -9
+lsof -i:8000 | grep 'TCP' | awk '{print $2}' | xargs kill -9
 python ./backend/gradio_app.py & python ./backend/register.py
