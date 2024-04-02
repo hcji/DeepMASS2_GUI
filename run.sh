7dca415b3030ec7c551751b07721553294e77712
cutexiaomian <2672458098@qq.com>
2024-04-02 17:13:40 +0800

    fix:修复自动代码更新功能

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 pwd
-git remote add origin https://github.com/hcji/DeepMASS2_GUI
-git pull origin lkr_dev:lkr_dev
-git reset --hard origin/lkr_dev
+#git remote add origin https://github.com/hcji/DeepMASS2_GUI
+#git pull origin lkr_dev:lkr_dev
+#git reset --hard origin/lkr_dev
 source activate deepmass2
 lsof -i:12341 | grep 'TCP' | awk '{print $2}' | xargs kill -9
 lsof -i:8000 | grep 'TCP' | awk '{print $2}' | xargs kill -9
